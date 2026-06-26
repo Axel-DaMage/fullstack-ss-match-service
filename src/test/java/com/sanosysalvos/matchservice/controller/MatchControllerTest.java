223dad5b99e03ce188cce9a823efd76d4600d8cf
NULL <axel.d4mage@gmail.com>
2026-06-25 23:30:50 -0400

    fix: replace @MockitoBean with @MockBean for Spring Boot 3.1.x compat

--- src/test/java/com/sanosysalvos/matchservice/controller/MatchControllerTest.java
+++ src/test/java/com/sanosysalvos/matchservice/controller/MatchControllerTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -24,7 +24,7 @@ class MatchControllerTest {
     @Autowired
     private MockMvc mvc;
 
-    @MockitoBean
+    @MockBean
     private MatchingService serviceMock;
 
     private final ObjectMapper mapper = new ObjectMapper();
